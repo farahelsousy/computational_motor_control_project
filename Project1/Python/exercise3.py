914f015d781c731cf45dc33e8f5c703ff54c2a8f
farah elsousy <farahelsousy@tsf-492-wpa-5-036.epfl.ch>
2025-04-10 14:27:22 +0200

    ex3

--- Project1/Python/exercise3.py
+++ Project1/Python/exercise3.py
@@ -6,7 +6,7 @@ import numpy as np
 import farms_pylog as pylog
 import matplotlib.pyplot as plt
 from plotting_common import plot_time_histories
-
+import plotting_common
 
 def exercise3():
 
@@ -22,11 +22,23 @@ def exercise3():
         compute_metrics=None,
         print_metrics=False,
         return_network=True,
-        headless=False,
+        headless=True,
         fast=False,
-        drive = 2.0  ,
-        weights_body2body = 15  ,
-        weights_body2body_contralateral = 5  
+        drive = 1,  # drive to the abstract oscillator controller
+        cpg_frequency_gain = 0.6,
+        cpg_frequency_offset = 0.6,
+        n_passive_joints = 2 ,
+        cpg_amplitude_gain = 0.125 * np.ones(13),  # amplitude gain for each joint
+        # Coupling weigths between neighborhood segments
+        weights_body2body = 30,
+        # Coupling weigths between contralateral segments
+        weights_body2body_contralateral = 10,
+        phase_lag_body = 2*np.pi,  # Total phase lag from body to tail along the spine
+        amplitude_rates = 20 , # Convergence rates of amplitudes
+
+
+        motor_output_scaling = 1  # muscle force scaling factor G
+
         
 
 
@@ -36,6 +48,12 @@ def exercise3():
     controller = run_single(
         all_pars
     )
+    plotting_common.plot_left_right(
+        controller.times,
+        controller.motor_out,
+        controller.motor_l,
+        controller.motor_r,
+    )
 
     # Hint: Optionally you can use some helper function to generate the plots
     # such as (plot_time_histories)
@@ -44,4 +62,5 @@ def exercise3():
 if __name__ == '__main__':
 
     exercise3()
+    plt.show()
 
