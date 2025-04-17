4c3898a3d8ef09af5868cef29cbec7315386c78b
Eiddavy <167872302+Eiddavy@users.noreply.github.com>
2025-04-17 14:43:12 +0200

    Update abstract_oscillator_controller.py
    this should be correct for the controller, coherent with the latest pdf

--- Project1/Python/controllers/abstract_oscillator_controller.py
+++ Project1/Python/controllers/abstract_oscillator_controller.py
@@ -4,144 +4,135 @@ import numpy as np
 import scipy.stats as ss
 from farms_core import pylog
 
-
 class AbstractOscillatorController:
-    """zebrafish controller"""
+    """zebrafish controller for the abstract oscillator CPG (Question 3)"""
 
-    def __init__(
-            self,
-            pars
-    ):
+    def __init__(self, pars):
         super().__init__()
 
         # Simulation parameters
         self.pars = pars
         self.n_iterations = pars.n_iterations
         self.timestep = pars.timestep
-        self.times = np.linspace(
-            0, self.pars.n_iterations*self.timestep, self.pars.n_iterations)
+        self.times = np.linspace(0, self.n_iterations * self.timestep, self.n_iterations)
 
-        # Abstract oscillator parameters
-        self.n_oscillators = 2*self.pars.n_joints
+        # Number of actively controlled joints (0 to 12); joints 13–14 remain passive
+        self.n_actuated_joints = 13
+        self.n_oscillators = 2 * self.n_actuated_joints   # 26 oscillators total
 
-        # States
-        self.n_eq = self.n_oscillators*2  # oscillator phase + oscillator amp
-        self.state = np.zeros([self.n_iterations, self.n_eq])
-        self.dstate = np.zeros([self.n_eq])  # derivative state
+        # State dimension: phases + amplitudes for each oscillator
+        self.n_eq = self.n_oscillators * 2
+        self.state = np.zeros((self.n_iterations, self.n_eq))
+        self.dstate = np.zeros(self.n_eq)
 
-        # State index
-        self.oscillator_phase_l = np.arange(0, self.pars.n_joints)
-        self.oscillator_phase_r = self.pars.n_joints + \
-            np.arange(0, self.pars.n_joints)
-        self.oscillator_phase_all = np.arange(0, 2*self.pars.n_joints)
-        self.oscillator_amplitude_l = self.pars.n_joints * \
-            2 + np.arange(0, self.pars.n_joints)
-        self.oscillator_amplitude_r = self.pars.n_joints * \
-            3 + np.arange(0, self.pars.n_joints)
-        self.oscillator_amplitude_all = self.pars.n_joints * \
-            2 + np.arange(0, 2*self.pars.n_joints)
+        # Indexing for phases and amplitudes
+        self.oscillator_phase_l     = np.arange(0, self.n_actuated_joints)
+        self.oscillator_phase_r     = self.n_actuated_joints + np.arange(0, self.n_actuated_joints)
+        self.oscillator_phase_all   = np.arange(0, 2 * self.n_actuated_joints)
+        self.oscillator_amplitude_l = 2 * self.n_actuated_joints + np.arange(0, self.n_actuated_joints)
+        self.oscillator_amplitude_r = 3 * self.n_actuated_joints + np.arange(0, self.n_actuated_joints)
+        self.oscillator_amplitude_all = 2 * self.n_actuated_joints + np.arange(0, 2 * self.n_actuated_joints)
 
-        # Initial state
+        # Initial phases: linearly spaced if not provided
         if self.pars.initial_phases is None:
-            self.state[0, :self.pars.n_joints] = 1 * \
-                np.linspace(2*np.pi, 0, self.pars.n_joints)
-            self.state[0, self.pars.n_joints:2 *
-                       self.pars.n_joints] = np.linspace(np.pi, -
-                                                         np.pi, self.pars.n_joints)
+            self.state[0, :self.n_actuated_joints] = np.linspace(2*np.pi, 0, self.n_actuated_joints)
+            self.state[0, self.n_actuated_joints:2*self.n_actuated_joints] = np.linspace(np.pi, -np.pi, self.n_actuated_joints)
         else:
-            self.state[0, :2*self.pars.n_joints] = self.pars.initial_phases
+            self.state[0, :2*self.n_actuated_joints] = self.pars.initial_phases
 
-        self.state[0, self.n_oscillators:2 *
-                   self.n_oscillators] = np.zeros(self.n_oscillators)
+        # Start amplitudes at zero (will ramp toward R_i)
+        self.state[0, self.n_oscillators:2*self.n_oscillators] = 0
 
-        # motor output and indexes
-        self.motor_out = np.zeros([self.n_iterations, self.n_oscillators])
-        self.motor_l = 2*np.arange(0, self.pars.n_joints)
+        # Motor output storage and mapping to left/right muscles
+        self.motor_out = np.zeros((self.n_iterations, self.n_oscillators))
+        self.motor_l = 2 * np.arange(0, self.n_actuated_joints)
         self.motor_r = self.motor_l + 1
 
-        # initialize ode solver
+        # ODE and step functions
         self.f = self.network_ode
         self.step = self.step_euler
 
-        # pre-computed zero activity for the last two tail joints
+        # Four zeros for the two passive joints (4 muscles)
         self.zeros4 = np.zeros(4)
 
     def network_ode(self, state):
-        """
-        pars
-        -------
-        self: AbstractOscillatorController
-            The controller object
-        state: <np.array>
-            An array of size 2*n_oscillators storing the oscillator phases and amplitudes
-        Returns
-        -------
-        dstate: <np.array>
-            An array of size 2*n_oscillators storing the oscillator phases and amplitudes derivatives
-            Note that phases and amplitudes have to appear in the same order as defined in states.
-        -------
-        This function is called each step to update the network states (amplitudes and phases).
-        Here you have to implement the Ordinary Differential Equation (ODE)
-        to compute the derivatives of network states.
-        For which you need CPG parameters  like nominal amplitudes, coupling weights, rates.
-        The computation of the above-mentioned parameters can go in another custom function or
-        be implemented here directly.
-        """
-        n_oscillators = self.n_oscillators
-        # Implement equation here
-        dphases = np.zeros(n_oscillators)
-        damplitudes = np.zeros(n_oscillators)
+        """Compute derivatives of phases and amplitudes."""
+        if state.ndim > 1:
+            current_state = state.flatten()
+        else:
+            current_state = state
+
+        n = self.n_oscillators
+        dphases = np.zeros(n)
+        damplitudes = np.zeros(n)
+
+        # Parameters
+        drive = self.pars.drive
+        f = self.pars.cpg_frequency_gain * drive + self.pars.cpg_frequency_offset
+        w_bb = self.pars.weights_body2body
+        w_bbc = self.pars.weights_body2body_contralateral
+        phi_lag = self.pars.phase_lag_body / (self.n_actuated_joints - 1) if self.n_actuated_joints > 1 else 0
+        amp_rate = self.pars.amplitude_rates
+        amp_gain = self.pars.cpg_amplitude_gain
+
+        for i in range(n):
+            θ_i = current_state[i]
+            r_i = current_state[i + n]
+
+            # coupling
+            coupling = 0
+            for j in range(n):
+                if i == j:
+                    continue
+                θ_j = current_state[j]
+                r_j = current_state[j + n]
+
+                if abs(i - j) == 2:
+                    w_ij, φ_ij = w_bb, np.sign(i - j) * phi_lag
+                elif (j - i == 1) and (i % 2 == 0):
+                    w_ij, φ_ij = w_bbc, -np.pi
+                else:
+                    w_ij, φ_ij = 0, 0
+
+                if w_ij != 0:
+                    coupling += w_ij * r_j * np.sin(θ_j - θ_i - φ_ij)
+
+            dphases[i] = 2 * np.pi * f + coupling
+
+            # nominal amplitude for oscillator i
+            if isinstance(amp_gain, (list, np.ndarray)):
+                joint_gain = amp_gain[i // 2]
+            else:
+                joint_gain = amp_gain
+            R_i = joint_gain * drive
+
+            damplitudes[i] = amp_rate * (R_i - r_i)
+
         return np.concatenate([dphases, damplitudes])
 
     def motor_output(self, iteration):
-        """
-        pars
-        -------
-        self: AbstractOscillatorController
-            The controller object
-            Hint: you can call self.state to access the current state of the controller
-        iteration: <int>
-            Current sim itertaion
-        Returns
-        -------
-        motor_output: <np.array>
-            An array of size 2*n_active_joints storing the muscle activations
-        -------
-        Here you have to use phase, amplitude and muscle strength to
-        finalize muscle activations for the first 13 active joints.
-        even indexes (0,2,4,...) = left muscle activations
-        odd indexes (1,3,5,...) = right muscle activations
+        """Compute muscle activations for active joints only."""
+        out = np.zeros(self.n_oscillators)
+        G = self.pars.motor_output_scaling
 
-        In addition to returning the motor output, store
-        them in self.motor_out for later use offline
-        Note: You only update and store the motor output at current iteration.
-        i.e. set only self.motor_out[iteration,:]
-        """
-        motor_output = np.zeros(self.n_oscillators)
-        self.motor_out[iteration, :] = motor_output
+        for i in range(self.n_actuated_joints):
+            θ_l = self.state[iteration, self.oscillator_phase_l[i]]
+            θ_r = self.state[iteration, self.oscillator_phase_r[i]]
 
-        return motor_output
+            # --- FIXED: use amplitude state directly, without extra * drive ---
+            r_l = self.state[iteration, self.oscillator_amplitude_l[i]]
+            r_r = self.state[iteration, self.oscillator_amplitude_r[i]]
+
+            out[self.motor_l[i]] = G * r_l * (1 + np.cos(θ_l))
+            out[self.motor_r[i]] = G * r_r * (1 + np.cos(θ_r))
+
+        self.motor_out[iteration, :] = out
+        return out
 
     def step_euler(self, iteration, timestep):
-        """
-        pars
-        -------
-        self: AbstractOscillatorController
-            The controller object
-            Hint: you can call self.state to access the current state of the controller
-                  you can call self.f(self, state) to call the network_ode(self, state) function
-        iteration: <int>
-            Current sim itertaion
-        Returns
-        -------
-        motor_output_all: <np.array>
-            An array of size 2*n_joints_total storing the muscle activations for the active and
-            passive joints at current sim itertaion
-        -------
-        Here you have to perform the Euler step on the oscillator states.
-        You return the muscle activation of all body joints at current iteration (array of 2*n_joints_total)
-        which includes updated motor outputs from active joints and the motor outputs for passive joints.
-        """
+        """Perform one Euler integration step and return full muscle activation vector."""
         self.state[iteration+1, :] = self.state[iteration, :]
-        return (np.zeros(30))
-
+        self.dstate = self.f(self.state[iteration, :])
+        self.state[iteration+1, :] += timestep * self.dstate
+        self.motor_output(iteration+1)
+        return np.concatenate([ self.motor_out[iteration+1, :], self.zeros4 ])
